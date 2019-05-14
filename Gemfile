f03bfd1365315626d602879dcce75408da86bfe9
PalaciosZ <criszelaya24@hotmail.com>
2019-05-13 22:56:47 -0600

    Creating view, connecting to api and showing into select the competitions

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'mime-types', '~> 3.2', '>= 3.2.2'
 gem 'netrc', '~> 0.11.0'
 gem 'http-accept', '~> 1.7'
 gem 'http-cookie', '~> 1.0', '>= 1.0.3'
+gem 'rest-client', '~> 2.0', '>= 2.0.2'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/selector_controller.rb
+++ app/controllers/selector_controller.rb
@@ -1,7 +1,19 @@
 require 'pry'
+require 'rest-client'
 class SelectorController < ApplicationController
+  URL = "https://api.football-data.org/v2/competitions/"
+  PREMIER_LEAGUE = 2021
+  LA_LIGA = 2014
+  SERIE_A = 2019
+  BUNDESLIGA = 2004
   def index
-    @leagues = {}
-    @leagues = 
+    @data = RestClient.get(URL,
+    headers= {accept: :json,'X-Auth-Token' => ENV['API_TOKEN']})
+    @leagues = JSON.parse(@data)
+  end
+
+  def show
+    binding.pry
+    @message = "Test"
   end
 end
