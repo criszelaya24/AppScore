3ac0c62c1900b0dc8b02d863907ae6f242d4b10f
criszelaya24@hotmail.com <criszelaya24@hotmail.com>
2019-05-15 22:40:09 -0600

    Extracting goals by time (first halftime and second halftime), missing style to present

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $("#leagues").change(function(){
         league = $(this).val();
-        console.log(league)
         $.ajax({
             url: '/get_teams',
             headers: {},
@@ -10,7 +9,6 @@ $(document).ready(function(){
             success: function(data){
                 $.each(data, function(key, value){
                     for (i = 0; i < value.length; ++i) {
-                        console.log(value[i]['name'])
                         let option = $("<option>").val(value[i]['id']).text(value[i]['name']);
                         $("#teams").append(option);
                     }
